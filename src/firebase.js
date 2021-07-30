bc33f364cbf0913a6613b5991db2830d95b52059
HongjianHuang <huanghongjian@live.cn>
2021-07-30 16:05:22 -0400

    create starter files

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/database";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZe-IvOGBRg4DBDEdKKdMV60AN0cLr9p0",
+  authDomain: "project-four-9cfa3.firebaseapp.com",
+  projectId: "project-four-9cfa3",
+  storageBucket: "project-four-9cfa3.appspot.com",
+  messagingSenderId: "968352751249",
+  appId: "1:968352751249:web:9877e7684c0226d31fe930",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;

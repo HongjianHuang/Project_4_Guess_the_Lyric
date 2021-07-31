d3bff40fcc743eccddff3fb8c2b13dfc64dba55c
HongjianHuang <huanghongjian@live.cn>
2021-07-31 12:54:02 -0400

    firebase worked!

--- src/Home.js
+++ src/Home.js
@@ -3,82 +3,62 @@ import { Link, Router } from "react-router-dom";
 import firebase from "./firebase";
 
 const Home = () => {
-  const [ questionInput, setQuestionInput ] = useState("")
-  const [ questionArray, setQuestionArray ] =useState([])
-  const poll = {yes: 0,
-                no: 0}
-
-  useEffect(() => {
-    //Variable that holds reference to database
-    const dbRef = firebase.database().ref();
-    // Event listener to variable dbRef; fires each time there is a change in value in database. Takes a callback function which will get data (response) from the database
-    dbRef.on("value", (response) => {
-      // Store response from query to firebase inside responseData variable
-      const responseData = response.val();
-      // Variable that stores the new state
-      const newStateArray = [];
-      // Local variable propertyName represents each of the properties or keys in responseData object
-      for (let propertyName in responseData) {
-        // New object is declared and is pushed into newStateArray
-        const Object = {
-          key: propertyName,
-          value: responseData[propertyName],
-        };
-        newStateArray.unshift(Object);
-      }
-      // Set new potluck list to state
-      console.log(newStateArray[0].key);
-      console.log(newStateArray);
-      setQuestionArray(newStateArray[0])
-    });
-  }, []);
-
+  const [questionInput, setQuestionInput] = useState("");
+  const [pollID, setPollID] = useState("");
+  //const [questionArray, setQuestionArray] = useState([]);
+  const poll = { yes: 0, no: 0 };
 
   const handleInputChange = (e) => {
     const { value } = e.target;
     console.log(value);
-    setQuestionInput(value)
-  }
-
+    setQuestionInput(value);
+  };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     console.log(e.target[0].value);
     setQuestionInput(e.target[0].value);
     console.log(questionInput);
     if (questionInput !== "") {
       const dbRef = firebase.database().ref();
-      dbRef.push([questionInput, poll]);
+      const pollID = dbRef.push([questionInput, poll]).key;
       setQuestionInput("");
+      setPollID(pollID);
     }
     // } else {
     //   alert("Enter a valid response");
     // }
-    console.log(questionArray);
-  }
+  };
 
-   // Function that handles changes in input elements on form
-   
+  // Function that handles changes in input elements on form
 
-
-
-    return (
-      <div>
-        <header>
-          <h1>Voting App</h1>
-        </header>
-        <main>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia libero expedita tenetur commodi voluptates repellat facilis provident odio ea necessitatibus!</p>
-        </main>
-        <form action="submit" onSubmit={handleSubmit}>
-          <label htmlFor="userquestionInput"></label>
-          <input type="text" name="userquestionInput" id="userquestionInput" onChange={handleInputChange}/>
-          <Link to={`${questionArray.key}`}>
-          <button type="submit">Create Poll</button>
-          </Link>
-        </form>
-      </div>
-    )
-}
+  return (
+    <div>
+      <header>
+        <h1>Voting App</h1>
+      </header>
+      <main>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+          libero expedita tenetur commodi voluptates repellat facilis provident
+          odio ea necessitatibus!
+        </p>
+      </main>
+      <form action="submit" onSubmit={handleSubmit}>
+        <label htmlFor="userquestionInput"></label>
+        <input
+          type="text"
+          name="userquestionInput"
+          id="userquestionInput"
+          onChange={handleInputChange}
+        />
+        <button type="submit">Create Poll</button>
+        <Link to={`${pollID}`}>
+          <button>Start the poll</button>
+        </Link>
+      </form>
+    </div>
+  );
+};
 
 export default Home;
--- src/Poll.js
+++ src/Poll.js
@@ -1,15 +1,77 @@
-import { useState } from 'react'
-
-
-
+import { useEffect, useState } from "react";
+import firebase from "./firebase";
+import Result from "./Result";
 const Poll = (props) => {
-    const pollID = props.match.params.pollID
-    console.log(pollID);
+  const pollID = props.match.params.pollID;
+  const [objectArray, setObjectArray] = useState([]);
+  const [vote, setVote] = useState("");
+  const [pollObject, setPollObject] = useState({});
+  const onChangeValue = (e) => {
+    setVote(e.target.value);
+  };
+  const handleClick = () => {
+    console.log(vote);
+    changeFirebaseValue(vote);
+  };
+  const changeFirebaseValue = (pollResult) => {
+    const dbRef = firebase.database().ref(pollID);
+    console.log(dbRef);
+    const pollObjectRef = pollObject;
+    console.log(pollObjectRef);
+    if (pollResult === "yes") {
+      //change the firebase value
+      pollObjectRef.value[1].yes = pollObjectRef.value[1].yes + 1;
+    } else if (pollResult === "no") {
+      pollObjectRef.value[1].no = pollObjectRef.value[1].no + 1;
+    }
+    dbRef.set(pollObjectRef.value);
+    //console.log(dbRef.push(pollObjectRef));
+  };
+  useEffect(() => {
+    //Variable that holds reference to database
+    const dbRef = firebase.database().ref();
+    // Event listener to variable dbRef; fires each time there is a change in value in database. Takes a callback function which will get data (response) from the database
+    dbRef.on("value", (response) => {
+      // Store response from query to firebase inside responseData variable
+      const responseData = response.val();
+      // Variable that stores the new state
+      //const newStateArray = [];
+      // Local variable propertyName represents each of the properties or keys in responseData object
 
+      for (let questionKey in responseData) {
+        // New object is declared and is pushed into newStateArray
 
-    return (
-        <h3>question</h3>
-    )
-}
+        const Object = {
+          key: questionKey,
+          value: responseData[questionKey],
+        };
+        if (Object.key === pollID) {
+          setObjectArray(Object.value);
+          setPollObject(Object);
+        }
+      }
+      // Set new potluck list to state
+      // console.log(newStateArray[0].key);
+      // console.log(newStateArray);
+      //setQuestionArray(newStateArray[0]);
+    });
+  }, []);
+  return (
+    <div className="question">
+      <h3>{objectArray[0]}</h3>
+      <div className="vote" onChange={onChangeValue}>
+        <label>
+          <input type="radio" value="yes" name="vote" /> Yes
+        </label>
+        <label>
+          <input type="radio" value="no" name="vote" /> No
+        </label>
+      </div>
+
+      <button onClick={handleClick}>vote</button>
+      <Result result={objectArray[1] ? objectArray[1] : { yes: 0, no: 0 }} />
+    </div>
+  );
+};
 
 export default Poll;

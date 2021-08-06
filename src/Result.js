c688ab623baf5f0660076193dd36bd433fcd211e
Sherry <sherry.t@live.ca>
2021-08-05 20:04:54 -0400

    removed percentage section and combined with results

--- src/Result.js
+++ src/Result.js
@@ -4,22 +4,14 @@ const Result = (props) => {
 
   return (
     <div className="result">
-      <h3>Result</h3>
+      <h3>Results</h3>
       { result ? Object.keys(result).map(function(item, i) {
         return(
-          <p key={i}>{item}: {result[item]} </p>
+          <p key={i}>{item}: {result[item]} ({((result[item] / sumValues) * 100).toFixed(1)}%)</p>
         )
         }) : null
       }
-      <p>Total: {sumValues}</p>
-
-      <h4>Percentage</h4>
-      { result ? Object.keys(result).map(function(item, i) {
-        return(
-          <p key={i}>{item}: {((result[item] / sumValues) * 100).toFixed(1)}%</p>
-        )
-        }) : null
-      }
+      <p>Total Votes: {sumValues}</p>
     </div>
   );
 };
